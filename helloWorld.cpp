f722b01fc68b32bb8744c6eb20c410718e21f034
Rajeev Negi <rajeevnegi005@gmail.com>
2025-08-22 12:38:19 +0530

    my first program

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main(){
+    // using \n
+    std::cout << "Hello world!\n2nd line\n";
+
+    // using std::endl
+    std::cout << "1st line" << std::endl;
+    std::cout << "2nd line";
+
+    return 0;
+}
