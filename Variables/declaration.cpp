3884e88757e09baa638bf6e5652839970bd8f48f
Rajeev Negi <rajeevnegi005@gmail.com>
2025-08-23 19:39:04 +0530

    added declarations.cpp

--- Variables/declaration.cpp
+++ Variables/declaration.cpp
@@ -2,5 +2,15 @@
 
 
 int main(){
-    
+    // list initialization
+    int a{4};
+
+    // direct initialization
+    int b(4);
+
+    // copy initialization
+    int c = 5;
+
+    // value initialization
+    int d {};
 }
